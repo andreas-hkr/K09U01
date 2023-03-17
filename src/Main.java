9b5f99d0bc1e2a7386b002cddabb7644b6552796
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 09:05:55 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Dagens mätarställning? 5673
+        // Mätarställning för ett år sedan? 4114
+        // Antal liter förbrukad bensin? 1325
+        // Antal körda mil:        1559
+        // Förbrukning per mil:    0,85
+
+
+    }
+}
