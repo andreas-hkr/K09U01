e72d66b1cccafe67ee0bdfe1f551068e2f9cd6d8
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 09:21:16 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Dagens mätarställning? 5673
@@ -6,6 +8,16 @@ public class Main {
         // Antal körda mil:        1559
         // Förbrukning per mil:    0,85
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Dagens mätarställning? ");
+        int now = input.nextInt();
+        System.out.print("Mätarställning för ett år sedan? ");
+        int then = input.nextInt();
+        System.out.print("Antal liter förbrukad bensin? ");
+        double amount = input.nextDouble();
 
+        int total = now - then;
+        System.out.format("Antal körda mil:%12d%n", total);
+        System.out.format("Förbrukning per mil:%,8.2f%n", amount/total);
     }
 }
